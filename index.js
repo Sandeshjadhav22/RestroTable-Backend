c4c8825fdb30b688510fdf03629db95ee303fd2f
sandy <sandeshjadhav2002@gmail.com>
2025-10-19 18:56:34 +0530

    db url added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express"
 import * as dotenv from "dotenv"
+import connectDB from "./Db/connect.js";
 dotenv.config()
 const app = express()
-const PORT= process.env.PORT ||8080;
+const PORT= process.env.PORT ||8000;
 
 
 app.use(express.json())
@@ -11,6 +12,11 @@ app.get("/",(req, res) => {
     res.json({ message: "Hello sandy" });
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Server Runnning on port ${PORT}`);
-})
+
+if (process.env.NODE_ENV !== "vercel") {
+  connectDB(process.env.MONGODB_URL);
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+  });
+}
